a231979470150a0fd5eb106168430109572287fd
github username <github email address>
2018-03-12 17:43:45 -0400

    Done.

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,1 +1,23 @@
-// Component Code Goes Here
+import React from 'react';
+
+class Address extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fullAddress: `${props.street}, ${props.city}`
+    }
+  }
+
+  render() {
+    return (
+      <div className="address">{this.props.street}, {this.props.city}</div>
+
+
+    );
+  }
+}
+// <div className="address">
+//   {this.state.fullAddress}
+// </div>   (would not render state if calling on computing two props. just call props)
+export default Address;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Address from './components/Address';
+
 ReactDOM.render(
-  <div>Components Need To Be Mounted</div>,
+  <Address
+    street="Santa Monica Blvd."
+    city="Santa Monica"
+  />,
   document.getElementById('root')
-)
+);
+// import ToggleButton from './components/ToggleButton';
+//
+// ReactDOM.render(
+//   <ToggleButton />,
+//   document.getElementById('root')
+// );
+
+// ReactDOM.render(
+//   <div>Components Need To Be Mounted</div>,
+//   document.getElementById('root')
+// )
